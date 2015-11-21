034ded83aef7b78b6c3478461562b468b4d0c9a8
Jonathan Huang <jon.hc.huang@gmail.com>
2015-11-21 17:34:19 -0500

    scaffold out graphs page

--- angular-flask/angular_flask/static/js/app.js
+++ angular-flask/angular_flask/static/js/app.js
@@ -12,23 +12,22 @@ angular.module('AngularFlask', ['angularFlaskServices'])
 			templateUrl: 'static/partials/about.html',
 			controller: AboutController
 		})
-		.when('/post', {
-			templateUrl: 'static/partials/post-list.html',
-			controller: PostListController
-		})
-		.when('/post/:postId', {
-			templateUrl: '/static/partials/post-detail.html',
-			controller: PostDetailController
-		})
+		// .when('/post', {
+		// 	templateUrl: 'static/partials/post-list.html',
+		// 	controller: PostListController
+		// })
+		// .when('/post/:postId', {
+		// 	templateUrl: '/static/partials/post-detail.html',
+		// 	controller: PostDetailController
+		// })
 		/* Create a "/blog" route that takes the user to the same place as "/post" */
-		.when('/blog', {
-			templateUrl: 'static/partials/post-list.html',
-			controller: PostListController
+		.when('/graphs', {
+			templateUrl: 'static/partials/graphs.html',
+			controller: GraphsController
 		})
 		.otherwise({
 			redirectTo: '/'
-		})
-		;
+		});
 
 		$locationProvider.html5Mode(true);
 	}])
--- angular-flask/angular_flask/static/js/controllers.js
+++ angular-flask/angular_flask/static/js/controllers.js
@@ -10,14 +10,18 @@ var AboutController = function($scope) {
 
 };
 
-var PostListController = function ($scope, Post) {
-	var postsQuery = Post.get({}, function(posts) {
-		$scope.posts = posts.objects;
-	});
+var GraphsController = function ($scope) {
+
 };
 
-var PostDetailController = function ($scope, $routeParams, Post) {
-	var postQuery = Post.get({ postId: $routeParams.postId }, function(post) {
-		$scope.post = post;
-	});
-};
+// var PostListController = function ($scope, Post) {
+// 	var postsQuery = Post.get({}, function(posts) {
+// 		$scope.posts = posts.objects;
+// 	});
+// };
+
+// var PostDetailController = function ($scope, $routeParams, Post) {
+// 	var postQuery = Post.get({ postId: $routeParams.postId }, function(post) {
+// 		$scope.post = post;
+// 	});
+// };
