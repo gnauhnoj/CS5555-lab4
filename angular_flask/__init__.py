b8fd9cf2d5b4a1eef373776396c3eebef3db62d7
Jonathan Huang <jon.hc.huang@gmail.com>
2015-11-26 00:17:43 -0800

    Move conversion out to init

--- angular_flask/__init__.py
+++ angular_flask/__init__.py
@@ -1,11 +1,15 @@
 import os
 import json
+import datetime
 from flask import Flask, request, Response
 from flask import render_template, send_from_directory, url_for
 from data_processing.loader import load_files
 
 app = Flask(__name__)
 data, FIRST_DATE, LAST_DATE = load_files()
+
+FIRST_DATE = datetime.date(FIRST_DATE.year, FIRST_DATE.month, FIRST_DATE.day)
+LAST_DATE = datetime.date(LAST_DATE.year, LAST_DATE.month, LAST_DATE.day)
 
 app.config.from_object('angular_flask.settings')
 
--- angular_flask/data_processing/analysis.py
+++ angular_flask/data_processing/analysis.py
@@ -5,9 +5,6 @@ from math import floor
 from scipy import stats
 from angular_flask import FIRST_DATE, LAST_DATE
 
-FIRST_DATE = datetime.date(FIRST_DATE.year, FIRST_DATE.month, FIRST_DATE.day)
-LAST_DATE = datetime.date(LAST_DATE.year, LAST_DATE.month, LAST_DATE.day)
-
 
 # calculate confidence interval
 def calculate_ci(data, level=0.95):
